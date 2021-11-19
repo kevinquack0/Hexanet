36062ac485bcd7cafa964b7a25b41dfc75f0b737
kevinquack0 <kevinbratislava99@gmail.com>
2021-11-19 11:10:55 -0700

    Little update

--- client-app/src/Features/activities/details/ActivityDetailsHeader.tsx
+++ client-app/src/Features/activities/details/ActivityDetailsHeader.tsx
@@ -93,7 +93,7 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
               disabled={activity.isCancelled}
               as={Link}
               to={`/manage/${activity.id}`}
-              color="black"
+              color="yellow"
               floated="right"
             >
               Manage Event
